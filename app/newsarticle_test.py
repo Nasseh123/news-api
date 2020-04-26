6cdafe5410fe96a14e404147c8bef059c00cbb4c
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-04-26 20:07:10 +0300

    created a news article and news source class where the blueprint of the objects to be fetched from the api are defined.DId a unnitest to both files and were succeful.DId a test to see if the instance was created succefulyy by the files and tested if the instance of an object was instantiated correctly marching the values given

---
+++ app/newsarticle_test.py
@@ -0,0 +1,30 @@
+import unittest
+from Models import news_article
+Newsarticle=news_article.Newsarticle
+
+class newsarticletest(unittest.TestCase):
+    """
+    Test Class to test the behaviour of the Newsarticle class
+    """
+
+    def setUp(self):
+        """
+         Set up method that will run before every Test
+        """
+
+        self.new_newsarticle=Newsarticle('author','title','description','www.news.com','urltoimage','2020-04-03T10:09:32Z','content')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_newsarticle,Newsarticle))
+
+    def test_init(self):
+        self.assertEqual(self.new_newsarticle.author,'author')
+        self.assertEqual(self.new_newsarticle.title,'title')
+        self.assertEqual(self.new_newsarticle.description,'description')
+        self.assertEqual(self.new_newsarticle.url,'www.news.com')
+        self.assertEqual(self.new_newsarticle.urlToImage,'urltoimage')
+        self.assertEqual(self.new_newsarticle.publishedAt,'2020-04-03T10:09:32Z')
+        self.assertEqual(self.new_newsarticle.content,'content')
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ app/newssource_test.py
@@ -0,0 +1,33 @@
+import unittest
+from Models import news_source
+Newssource=news_source.Newssource
+
+class newstest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Newssource class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_newssource=Newssource('id','name','description','www.abcnews.go.com','category','language','country')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_newssource,Newssource))
+
+    def test_init(self):
+        """
+          test_init test case to test if the object is initialized properly
+        """
+        self.assertEqual(self.new_newssource.id,'id')
+        self.assertEqual(self.new_newssource.name,'name')
+        self.assertEqual(self.new_newssource.description,'description')
+        self.assertEqual(self.new_newssource.url,'www.abcnews.go.com')
+        self.assertEqual(self.new_newssource.category,'category')
+        self.assertEqual(self.new_newssource.language,'language')
+        self.assertEqual(self.new_newssource.country,'country')
+        
+
+if __name__ == '__main__':
+    unittest.main()
