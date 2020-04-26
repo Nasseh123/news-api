7c6348314959efb0aa2df11d9cb27a850c97edb6
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-04-26 16:48:43 +0300

    Created the configurations classes.They are cong which is the parent ,prodCnfig used during production and devConfig used during development.THen imported the configurations to the init file so as to make them to be usable by the application.Created the control flows in the index and news files where variables defined from the views file in there respective functions where passed to the html files to represents the head.Then created a control file for the files where it picked an alternative of the title if it was not defined in the views file.

--- app/views.py
+++ app/views.py
@@ -8,11 +8,13 @@ def index():
     View root page function that returns the index page and its data
     '''
     message='Hello World '
-    return render_template('index.html',message=message)
+    title="Home-Welcome to NEWS sources Website"
+    return render_template('index.html',title=title)
 
 @app.route('/news/<news_id>')
 def news(news_id):
     '''
     View news article function that returns the news articles page and its data
     '''
-    return render_template ('news.html',id=news_id)
+    title=news_id
+    return render_template ('news.html',id=news_id,title=title)
