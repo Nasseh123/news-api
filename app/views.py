a11a3406ffed1c74b58987ee783f1a3088d24f5a
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-04-26 16:22:18 +0300

    Created the main homepage and the news article page.The main homepage is responsible for holding t=all the news sources and links to ther respectives news articles.The news article page is responsible for holding all the articles and the links to the main pages etc.Created this 2 views in the views.py file and run the application to see if it works by passing the instance to the run.py file so as to lunch a server and run an address to be used in the browser.

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from flask import render_template
+from app import app
+
+# Views
+@app.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    message='Hello World '
+    return render_template('index.html',message=message)
+
+@app.route('/news/<news_id>')
+def news(news_id):
+    '''
+    View news article function that returns the news articles page and its data
+    '''
+    return render_template ('news.html',id=news_id)
